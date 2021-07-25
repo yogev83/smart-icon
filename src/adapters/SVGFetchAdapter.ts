db5e4da59b914f6652415f0ace67dae0a9e89e6e
erictooth <1023110+erictooth@users.noreply.github.com>
2021-07-25 20:42:57 +0000

    feat: add `querySelector` option to SVGFetchAdapter for CORS SVG sprite usage

--- src/adapters/SVGFetchAdapter.ts
+++ src/adapters/SVGFetchAdapter.ts
@@ -1,18 +1,39 @@
 import { SmartIconOptions, EventBus } from "../types";
 import { BaseAdapter } from "../BaseAdapter";
 
-export const SVGFetchAdapter = () => (config: SmartIconOptions, eventBus: EventBus) => {
-    return class SVGFetchAdapter extends BaseAdapter(config, eventBus) {
-        getSvgText(): Promise<string> {
-            return fetch(this.getPath()).then((res) => res.text());
-        }
-        generateTemplate() {
-            return this.getSvgText();
-        }
-        async update() {
-            if (this.shadowRoot) {
-                this.shadowRoot.innerHTML = await this.generateTemplate();
-            }
-        }
-    };
+type SVGFetchAdapterOptions = {
+    querySelector?: (name: string) => string;
 };
+
+export const SVGFetchAdapter =
+    (options: SVGFetchAdapterOptions = {}) =>
+    (config: SmartIconOptions, eventBus: EventBus) => {
+        return class SVGFetchAdapter extends BaseAdapter(config, eventBus) {
+            async getSvgText(): Promise<string> {
+                const svgText = await fetch(this.getPath()).then((res) => res.text());
+
+                if (!options.querySelector) {
+                    return svgText;
+                }
+
+                const fragment = document
+                    .createRange()
+                    .createContextualFragment(svgText)
+                    .querySelector(options.querySelector(this.name));
+
+                const viewBox = fragment?.getAttribute("viewBox") || "";
+
+                return `<svg xmlns="http://www.w3.org/2000/svg" width="100%" height="100%" ${
+                    (viewBox && `viewBox="${viewBox}"`) || ""
+                }>${fragment?.innerHTML}</svg>`;
+            }
+            generateTemplate() {
+                return this.getSvgText();
+            }
+            async update() {
+                if (this.shadowRoot) {
+                    this.shadowRoot.innerHTML = await this.generateTemplate();
+                }
+            }
+        };
+    };
